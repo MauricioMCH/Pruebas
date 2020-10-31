ce7eaa4e273802fdbbe0f551768077c29ff3bc12
MauricioMCH <71183060+MauricioMCH@users.noreply.github.com>
2020-10-30 20:52:22 -0600

    Update autenticar.blade.php

--- resources/views/autenticar.blade.php
+++ resources/views/autenticar.blade.php
@@ -1,22 +1,62 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Entrada</title>
-</head>
+
+<html>
+
+
+<nav class="navbar navbar-succes bg-success">
+  <a class="navbar-brand" href="#">
+    Frameworks
+  </a>
+</nav>
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<div class="container">
+<div class="starter-template">
+<br><br><br>
+
+<div class ="row">
+<div class="col-md-4 col-md-offset-4">
+<br><br>
+
 <body>
-    @if (session('error'))
-    <div>
-        {{ session('error') }}
-    </div>
-    <br>
-    @endif
-    <form action="/validar" method="post">
-        @csrf
-        Usuario:   <input type="text" name="usuario"><br>
-        Contraseña:<input type="password" name="password"><br>
-        <input type="submit" value="Enviar">
-    </form>
+....@if (session('error'))
+.....<div>
+ ..... {{session('error') }}
+.....</div>
+<br>
+@endif
+
+<div class="panel panel-default">
+   <div class="panel-body">
+ <!--Estructura de nuestro login-->
+
+   <legend>Login</legend>
+
+<form  action="validar" method="post" role="form">
+    @csrf
+  <!--Por medio de un formulario con el metodo GET haremos que nuestros imput
+      se envien a usuario.php para que pasen por las condiciones-->
+<br>
+
+<div class="form-group">
+    <label>Usuario</label>
+    <input type="text" REQUIRED name="usuario"  class="form-control" autofocus requiered placeholder="Ingrese usuario">
+</div>
+
+<div class="form-group">
+    <label>Contraseña</label>
+    <input type="password"  name="password" class="form-control"  placeholder="****">
+</div>
+
+<button type="submit"  class="btn btn-success">Ingresar</button>
+<!--Boton tipo submit para accionar el formulario-->
+</form>
+   </div>
+</div>
+
+</div>
+</div>
+</div>
+</div>
 </body>
+
 </html>
